820e7e1984637de2a49bc9cea469202058fc102f
Lynn Cyrin <firemagelynn@gmail.com>
2015-12-14 21:40:43 -0800

    cleanup routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'index/index'
+  resources :blocks
+  resources :reports
+  resources :users
 
-  resources :users
-  resources :users
-  resources :blocks
-  resources :blocks
-  resources :users
-  resources :blocks
-  resources :blocks
-  root 'welcome#index'
-
+  root 'index#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
