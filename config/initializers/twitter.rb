88dce1f97c36f28615c389bf23f1dc030894d11e
Lynn Cyrin <firemagelynn@gmail.com>
2015-12-21 19:19:39 -0800

    reformat initializer
    idk how I would have done the previous format with Heroku

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,15 +1,21 @@
 # looks the following in secrets.yml
 #
 # development:
-#   twitter:
-#     consumer_key: XXXXXXXXXXXXXXXXXXXXXXXXXXX
-#     consumer_secret: XXXXXXXXXXXXXXXXXXXXXXXX
-#     access_token: XXXXXXXXXXXXXXXXXXXXXXXXXXX
-#     access_token_secret: XXXXXXXXXXXXXXXXXXXX
+#   consumer_key: XXXXXXXXXXXXXXXXXXXXXXXXXXX
+#   consumer_secret: XXXXXXXXXXXXXXXXXXXXXXXX
+#   access_token: XXXXXXXXXXXXXXXXXXXXXXXXXXX
+#   access_token_secret: XXXXXXXXXXXXXXXXXXXX
 
 class TwitterClient
     cattr_accessor :REST, :Stream
 end
 
-TwitterClient.REST = Twitter::REST::Client.new(Rails.application.secrets.twitter)
-TwitterClient.Stream = Twitter::Streaming::Client.new(Rails.application.secrets.twitter)
+twitterSecrets = {
+    consumer_key: Rails.application.secrets.consumer_key,
+    consumer_secret: Rails.application.secrets.consumer_secret,
+    access_token: Rails.application.secrets.access_token,
+    access_token_secret: Rails.application.secrets.access_token_secret,
+}
+
+TwitterClient.REST = Twitter::REST::Client.new(**twitterSecrets)
+TwitterClient.Stream = Twitter::Streaming::Client.new(**twitterSecrets)
