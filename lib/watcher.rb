5e57713fd63c5e8206cfac71f920e552af35df30
Lynn Cyrin <firemagelynn@gmail.com>
2015-12-22 00:56:10 -0800

    watcher uses UserStream

--- lib/watcher.rb
+++ lib/watcher.rb
@@ -1,6 +1,15 @@
-TwitterClient.Stream.filter(track: 'minecraft') do |object|
-  if object.is_a?(Twitter::Tweet)
-    puts object.text
+$stdout.sync = true
+
+TwitterClient.Stream.user do |object|
+  case object
+  when Twitter::Tweet
+    puts('[Adding to Reports] ' + object.text)
     Report.create(text: object.text)
+  when Twitter::DirectMessage
+    puts('[Adding to Reports] ' + object.text)
+    Report.create(text: object.text)
+  when Twitter::Streaming::StallWarning
+    warn "Falling behind!"
   end
+
 end
