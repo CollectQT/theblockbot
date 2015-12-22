c873d20b74c51187e264ae7881f63e810e618e40
Lynn Cyrin <firemagelynn@gmail.com>
2015-12-21 20:09:16 -0800

    better namespacing for twitter keys

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,20 +1,20 @@
 # looks the following in secrets.yml
 #
 # development:
-#   consumer_key: XXXXXXXXXXXXXXXXXXXXXXXXXXX
-#   consumer_secret: XXXXXXXXXXXXXXXXXXXXXXXX
-#   access_token: XXXXXXXXXXXXXXXXXXXXXXXXXXX
-#   access_token_secret: XXXXXXXXXXXXXXXXXXXX
+#   twitter_consumer_key: XXXXXXXXXXXXXXXXXXXXXXXXXXX
+#   twitter_consumer_secret: XXXXXXXXXXXXXXXXXXXXXXXX
+#   twitter_access_token: XXXXXXXXXXXXXXXXXXXXXXXXXXX
+#   twitter_access_token_secret: XXXXXXXXXXXXXXXXXXXX
 
 class TwitterClient
     cattr_accessor :REST, :Stream
 end
 
 twitterSecrets = {
-    consumer_key: Rails.application.secrets.consumer_key,
-    consumer_secret: Rails.application.secrets.consumer_secret,
-    access_token: Rails.application.secrets.access_token,
-    access_token_secret: Rails.application.secrets.access_token_secret,
+    consumer_key: Rails.application.secrets.twitter_consumer_key,
+    consumer_secret: Rails.application.secrets.twitter_consumer_secret,
+    access_token: Rails.application.secrets.twitter_access_token,
+    access_token_secret: Rails.application.secrets.twitter_access_token_secret,
 }
 
 TwitterClient.REST = Twitter::REST::Client.new(**twitterSecrets)
