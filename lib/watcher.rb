35e6f3db1d693ea8f90288559df679c0a1098c20
Lynn Cyrin <firemagelynn@gmail.com>
2015-12-23 19:41:11 -0800

    Revert "watcher uses UserStream"
    This reverts commit 5e57713fd63c5e8206cfac71f920e552af35df30.
    
    Going to try using track instead

--- lib/watcher.rb
+++ lib/watcher.rb
@@ -1,15 +1,6 @@
-$stdout.sync = true
-
-TwitterClient.Stream.user do |object|
-  case object
-  when Twitter::Tweet
-    puts('[Adding to Reports] ' + object.text)
+TwitterClient.Stream.filter(track: 'minecraft') do |object|
+  if object.is_a?(Twitter::Tweet)
+    puts object.text
     Report.create(text: object.text)
-  when Twitter::DirectMessage
-    puts('[Adding to Reports] ' + object.text)
-    Report.create(text: object.text)
-  when Twitter::Streaming::StallWarning
-    warn "Falling behind!"
   end
-
 end
