befbb54c6bece3e98ebbf988b047786c37817e76
Lynn Cyrin <firemagelynn@gmail.com>
2015-12-23 22:20:14 -0800

    move Report parsing into model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,24 @@
 class Report < ActiveRecord::Base
   belongs_to :block_list
+
+  def self.parse(text)
+    puts '[Incoming Report] '+text.squish
+
+    text_included_a_list = false
+
+    for blocklist in BlockList.all()
+      if ('#'+blocklist.name).downcase.in? text.downcase
+        puts '[Created Report('+blocklist.name+')] '+text.squish
+        Report.create(text: text, block_list_id: blocklist.id)
+        text_included_a_list = true
+      end
+    end
+
+    # unless text_included_a_list
+    #   blocklist = BlockList.find(name: 'General')
+    #   puts '[Created Report('+blocklist.name+')] '+text.squish
+    #   Report.create(text: text, block_list_id: blocklist.id)
+    # end
+  end
+
 end
--- lib/watcher.rb
+++ lib/watcher.rb
@@ -7,29 +7,8 @@ track = 'lynncyrin #block'
 
 puts 'Starting Watcher for "'+track+'"'
 
-def report(text)
-  puts '[Incoming Report] '+text.squish
-
-  text_included_a_list = false
-
-  for blocklist in BlockList.all()
-    if ('#'+blocklist.name).downcase.in? text.downcase
-      puts '[Created Report('+blocklist.name+')] '+text.squish
-      Report.create(text: text, block_list_id: blocklist.id)
-      text_included_a_list = true
-    end
-  end
-
-  # unless text_included_a_list
-  #   blocklist = BlockList.find(name: 'General')
-  #   puts '[Created Report('+blocklist.name+')] '+text.squish
-  #   Report.create(text: text, block_list_id: blocklist.id)
-  # end
-
-end
-
 TwitterClient.Stream.filter(track: track) do |object|
   if object.is_a? Twitter::Tweet
-    report object.text
+    Report.parse(object.text)
   end
 end
