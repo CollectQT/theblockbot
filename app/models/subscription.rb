7b1e3d685cda958c876c4f245fb4cd6cc6d1682d
Lynn Cyrin <firemagelynn@gmail.com>
2015-12-26 23:21:04 -0800

    can set target user for subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,8 +3,8 @@ class Subscription < ActiveRecord::Base
   belongs_to :block_list
   has_many :blocks
 
-  def self.to(block_list_id)
-    Subscription.create(user_id: @current_user.id, block_list_id: block_list_id)
+  def self.to(block_list_id, user_id=@current_user.id)
+    Subscription.create(user_id: user_id, block_list_id: block_list_id)
   end
 
 end
