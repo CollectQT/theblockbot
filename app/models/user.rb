480b74afb7a613379b176bb78a4f5fe0cdc0f631
Lynn Cyrin <firemagelynn@gmail.com>
2016-01-02 14:17:26 -0800

    slightly more resiliant User.get

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,24 @@
 class User < ActiveRecord::Base
   has_one :auth, :dependent => :destroy
   has_many :subscriptions
-  has_many :reports
   has_many :block_lists, through: :subscriptions
+  has_many :reports_by, class_name: "Report", foreign_key: "reporter_id"
+  has_many :reports_against, class_name: "Report", foreign_key: "target_id"
 
   validates :website, presence: true
   validates :account_id, presence: true
   validates_uniqueness_of :account_id, scope: :website
 
   def self.get(_user, website='twitter')
+    # takes in a user object from an API library (ex: the Twitter gem)
+    # returns a local User instance
+
+    # sometimes we already have a user object, and aren't aware of it
+    if _user.is_a? User
+      return _user
 
     # https://dev.twitter.com/overview/api/users
-    if website == 'twitter'
+    elsif website == 'twitter'
       User.find_or_create_by(account_id: _user.id.to_s, website: website) do |user|
         user.name                  = _user.name
         user.user_name             = _user.screen_name
