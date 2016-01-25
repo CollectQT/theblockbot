3d7668af0e979eea32cfbbf9a9880a410f81c2e8
Lynn Cyrin <firemagelynn@gmail.com>
2016-01-25 10:02:26 -0800

    no more general list

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ me = User.get(TwitterClient.REST.user)
 list1 = BlockList.create(name: 'Bad Cats')
 list2 = BlockList.create(name: 'Dogs', hidden: true)
 list3 = BlockList.create(name: 'Birbs')
-list4 = BlockList.create(name: 'General')
 
 Blocker.create(block_list: list3, user: me)
 Admin.create(block_list: list4, user: me)
