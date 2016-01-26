9b461769dfc74a78efc1a7ee9c83f5e0bab11ac8
Lynn Cyrin <firemagelynn@gmail.com>
2016-01-26 11:20:22 -0800

    resolve issue from previous commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ list1 = BlockList.create(name: 'Bad Cats')
 list2 = BlockList.create(name: 'Dogs', hidden: true)
 list3 = BlockList.create(name: 'Birbs')
 
-Blocker.create(block_list: list3, user: me)
-Admin.create(block_list: list4, user: me)
+Blocker.create(block_list: list1, user: me)
+Admin.create(block_list: list2, user: me)
 
 Report.parse('@lynncyrin #block +cyrin_test_2 #badcats knocked over my water', me)
 
