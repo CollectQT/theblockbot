f483a9df375ab57f74e8f6a5948535158f221cc9
Lynn Cyrin <firemagelynn@gmail.com>
2016-01-26 21:30:21 -0800

    unblocks process per hour, instead of per day

--- config/clock.rb
+++ config/clock.rb
@@ -2,5 +2,5 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
-  every(1.days, 'UnblockProcessor') { UnblockProcessor.perform_async }
+  every(1.hours, 'UnblockProcessor') { UnblockProcessor.perform_async }
 end
