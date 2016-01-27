50ee9cfe1a15d16dd7d3fad80685a6f0b11dc8cb
Lynn Cyrin <firemagelynn@gmail.com>
2016-01-26 19:54:51 -0800

    swap route order to be specific > generic

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   post   '/block_lists/:id/subscribe' => 'block_lists#subscribe',   :as => :list_subscribe
   delete '/block_lists/:id/subscribe' => 'block_lists#unsubscribe', :as => :list_unsubscribe
 
-  get    'reports'             => 'reports#index',   :as => :reports
-  get    'reports/:id'         => 'reports#show',    :as => :report
-  get    'reports/new'         => 'reports#new',     :as => :report_new
-  post   'reports'             => 'reports#create',  :as => :report_create
   post   'reports/:id/approve' => 'reports#approve', :as => :report_approve
+  get    'reports/new'         => 'reports#new',     :as => :report_new
+  get    'reports/:id'         => 'reports#show',    :as => :report
   delete 'reports/:id'         => 'reports#deny',    :as => :report_deny
+  post   'reports'             => 'reports#create',  :as => :report_create
+  get    'reports'             => 'reports#index',   :as => :reports
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', :as => :signout
