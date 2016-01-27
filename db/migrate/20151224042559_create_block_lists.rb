9c69d34e3817697524522286258f3414de29f6f7
Lynn Cyrin <firemagelynn@gmail.com>
2016-01-26 16:11:43 -0800

    Block Lists have descriptions

--- db/migrate/20151224042559_create_block_lists.rb
+++ db/migrate/20151224042559_create_block_lists.rb
@@ -2,6 +2,7 @@ class CreateBlockLists < ActiveRecord::Migration
   def change
     create_table :block_lists do |t|
       t.string :name, null: false
+      t.string :description
       t.boolean :hidden, null: false, default: false
       t.boolean :show_blocks, null: false, default: true
       t.integer :expires, null: false, default: 31*12
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 me = User.get(TwitterClient.REST.user)
 
-list1 = BlockList.create(name: 'Bad Cats')
-list2 = BlockList.create(name: 'Dogs', hidden: true)
-list3 = BlockList.create(name: 'Birbs')
+list1 = BlockList.create(name: 'Bad Cats', description: 'mostly they knocked things over')
+list2 = BlockList.create(name: 'Dogs', description: 'v smells', hidden: true)
+list3 = BlockList.create(name: 'Birbs', description: 'stole all the bread')
 
 Blocker.create(block_list: list1, user: me)
 Admin.create(block_list: list2, user: me)
