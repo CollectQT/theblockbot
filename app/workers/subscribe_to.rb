280a453cd736c3e73dec1f025e92ff942204176f
Lynn Cyrin <firemagelynn@gmail.com>
2016-01-26 22:19:20 -0800

    modifying subscription is synchronous

--- app/workers/subscribe_to.rb
+++ app/workers/subscribe_to.rb
@@ -2,12 +2,9 @@ class SubscribeTo
   include Sidekiq::Worker
 
   def perform(user_id, block_list_id)
-    Subscription.create(user_id: user_id, block_list_id: block_list_id)
-
     for report in Report.where(approved: true, expired: false, block_list_id: block_list_id)
       CreateBlock.perform_async(user_id, report.id)
     end
-
   end
 
 end
--- app/workers/unsubscribe_from.rb
+++ app/workers/unsubscribe_from.rb
@@ -4,13 +4,9 @@ class UnsubscribeFrom
   def perform(user_id, block_list_id)
     user_model = User.find(user_id)
     user_client = TwitterClient.user(user_model.auth)
-
-    Subscription.find_by(user_id: user_id, block_list_id: block_list_id).delete
-
     for block in Block.where(user_id: user_id, block_list_id: block_list_id)
       Unblock.perform_async(user_id, block.target.account_id.to_i, block.id)
     end
-
   end
 
 end
