8b1014512a90dd113a2b180b52b3a8b09e1d268b
Lynn Cyrin <firemagelynn@gmail.com>
2016-04-15 00:57:16 -0700

    better database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,16 @@
-# For testing! Not actual seed values!
-
 me = User.get(TwitterClient.REST.user)
 
-list1 = BlockList.create(
-  name: 'Bad Cats',
-  description: 'mostly they knocked things over',
-  expires: 365)
-list2 = BlockList.create(
-  name: 'Dogs',
-  description: 'v smells',
-  expires: nil)
-list3 = BlockList.create(
-  name: 'Birbs',
-  description: 'stole all the bread, list is softblocks only',
-  expires: 0,
-  showcase: true)
+list_1 = BlockList.create(
+  name: 'TestNoExpire',
+  description: 'for testing and maintenence',
+  expires: nil,)
+list_2 = BlockList.create(
+  name: 'TestSoftBlocks',
+  description: 'for testing and maintenence',
+  expires: 0,)
 
-Admin.create(block_list: list1, user: me)
-Admin.create(block_list: list2, user: me)
-Admin.create(block_list: list3, user: me)
+Admin.create(block_list: list_1, user: me)
+Admin.create(block_list: list_2, user: me)
 
-Subscription.add(me.id, list1.id)
-Subscription.add(me.id, list2.id)
-Subscription.add(me.id, list3.id)
-
-Report.parse('@lynncyrin #block +cyrin_test_2 #badcats knocked over my water', me)
-Report.parse('@lynncyrin #block +cyrin_test_2 #dogs knocked over my water', me)
-Report.parse('@lynncyrin #block +cyrin_test_2 #birbs knocked over my water', me)
+Report.parse('@lynncyrin #block +cyrin_test_2 #testsoftblocks testing', me).approve(me)
+Report.parse('@lynncyrin #block +cyrin_test_2 #testnoexpire testing', me).approve(me)
