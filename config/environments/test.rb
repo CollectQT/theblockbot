ebc2d8c7bbd952452eca7ebaf736818128634fe0
Lynn Cyrin <firemagelynn@gmail.com>
2016-04-20 00:17:31 -0700

    dont cache test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.static_cache_control = 'public, max-age=3600'
 
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
