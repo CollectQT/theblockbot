5cd6b4b9fd5e2f50c0f5aa26ecac314f7c452632
Lynn Cyrin <firemagelynn@gmail.com>
2016-04-19 22:09:08 -0700

    enable caching in all environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,9 +16,8 @@ Rails.application.configure do
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
 
-  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
