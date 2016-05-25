f6ad81ef0fcdb8ac5bae5f43714237321e6f84e7
Lynn Cyrin <firemagelynn@gmail.com>
2016-05-24 20:18:47 -0700

    Softblocks / force unfollows

---
+++ app/workers/post_softblock.rb
@@ -0,0 +1,16 @@
+class PostSoftblock
+  include Sidekiq::Worker
+
+  sidekiq_retry_in do |count|
+    # 15min * 60sec/min = 900sec
+    900 * (count + 1)
+  end
+
+  def perform(user_database_id, target_twitter_id)
+    user = TwitterClient.user(User.find(user_database_id))
+
+    user.block(target_twitter_id)
+    user.unblock(target_twitter_id)
+  end
+
+end
