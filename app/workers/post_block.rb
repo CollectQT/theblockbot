366d352a0aa03724b6e3d2403d15172df8d62227
Lynn Cyrin <firemagelynn@gmail.com>
2016-05-24 19:28:46 -0700

    retry_in 15minutes

--- app/workers/post_block.rb
+++ app/workers/post_block.rb
@@ -1,6 +1,11 @@
 class PostBlock
   include Sidekiq::Worker
 
+  sidekiq_retry_in do |count|
+    # 15min * 60sec/min = 900sec
+    900 * (count + 1)
+  end
+
   def perform(user_id, report_id)
     user = TwitterClient.user(User.find(user_id))
     report = Report.find(report_id)
--- app/workers/post_unblock.rb
+++ app/workers/post_unblock.rb
@@ -1,6 +1,11 @@
 class PostUnblock
   include Sidekiq::Worker
 
+  sidekiq_retry_in do |count|
+    # 15min * 60sec/min = 900sec
+    900 * (count + 1)
+  end
+
   def perform(user_id, target_id, block_id)
     TwitterClient.user(User.find(user_id)).unblock(target_id)
     Block.find(block_id).delete
