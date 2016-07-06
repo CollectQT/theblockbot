a4958cc59bb064c8f46140652d07fd41b248d31f
Lynn Cyrin <firemagelynn@gmail.com>
2016-07-06 16:45:53 -0700

    Make FoF page a private method, take argument for page count

--- app/workers/read_friends_or_followers.rb
+++ app/workers/read_friends_or_followers.rb
@@ -1,6 +1,6 @@
 class ReadFriendsOrFollowers
 
-  def read(type, user_id)
+  def read(type, user_id, count)
     Rails.cache.fetch("fof/all/#{type}/#{user_id}", expires_in: 1.days) do
       user = TwitterClient.user(User.find(user_id))
       cursor = -1
@@ -8,7 +8,7 @@ class ReadFriendsOrFollowers
 
       if ['following', 'followers', 'friends'].include? type
         while cursor != 0
-          fof_page, cursor = page(type, user, cursor)
+          fof_page, cursor = page(type, user, cursor, count)
           fof_all += fof_page
         end
       end
@@ -18,13 +18,13 @@ class ReadFriendsOrFollowers
     end
   end
 
-  def page(type, user, cursor)
+  private def page(type, user, cursor, count)
     Rails.cache.fetch("fof/page/#{type}/#{user.user.id}/#{cursor}", expires_in: 1.days) do
 
       puts "fof/page/#{type}/#{user.user.id}/#{cursor}"
 
       if type == 'followers'
-        response = user.follower_ids(:cursor => cursor, :count => 500)
+        response = user.follower_ids(:cursor => cursor, :count => count)
       elsif type == 'following' or type == 'friends'
         response = user.friend_ids(:cursor => cursor)
       end
