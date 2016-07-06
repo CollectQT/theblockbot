00f3d786e5677f7d5a11bf7cb3df252cd9865a34
Lynn Cyrin <firemagelynn@gmail.com>
2016-07-05 22:04:35 -0700

    spacing

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -2,11 +2,11 @@ describe "create_blocks_from_report" do
 
   let(:worker) { CreateBlocksFromReport.new }
   let(:report) { instance_double(Report, id: 1, block_list:
-      instance_double(BlockList, users: [
-        instance_double(User, id: 1),
-        instance_double(User, id: 1),
-        instance_double(User, id: 1),
-      ])
+    instance_double(BlockList, users: [
+      instance_double(User, id: 1),
+      instance_double(User, id: 1),
+      instance_double(User, id: 1),
+    ])
   )}
 
   it "creates 3 blocks for 3 block list subscribers from an approved report" do
