93591d96b14f2bfbed855a8a1713de4d89947a58
Lynn Cyrin <firemagelynn@gmail.com>
2016-07-17 02:12:37 -0700

    env keys are usually in capslock

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,14 +1,14 @@
 TwitterClient = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['twitter_consumer_key']
-  config.consumer_secret     = ENV['twitter_consumer_secret']
-  config.access_token        = ENV['twitter_access_token']
-  config.access_token_secret = ENV['twitter_access_token_secret']
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
 TweetStream.configure do |config|
-  config.consumer_key       = ENV['twitter_consumer_key']
-  config.consumer_secret    = ENV['twitter_consumer_secret']
-  config.oauth_token        = ENV['twitter_access_token']
-  config.oauth_token_secret = ENV['twitter_access_token_secret']
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
   config.auth_method = :oauth
 end
