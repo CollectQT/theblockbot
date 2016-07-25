a86d81d433af2d25f3cb6b07f96828c3de9c67d6
Lynn Cyrin <firemagelynn@gmail.com>
2016-07-24 17:36:31 -0700

    leading slash on routes (for consistency)

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,16 @@ Rails.application.routes.draw do
   post   '/block_lists/:id/subscribe'    => 'block_lists#subscribe',   :as => :list_subscribe
   delete '/block_lists/:id/subscribe'    => 'block_lists#unsubscribe', :as => :list_unsubscribe
 
-  post   'reports/:id/approve' => 'reports#approve', :as => :report_approve
-  get    'reports/new'         => 'reports#new',     :as => :report_new
-  get    'reports/:id'         => 'reports#show',    :as => :report
-  delete 'reports/:id'         => 'reports#deny',    :as => :report_deny
-  post   'reports'             => 'reports#create',  :as => :report_create
-  get    'reports'             => 'reports#index',   :as => :reports
+  post   '/reports/:id/approve' => 'reports#approve', :as => :report_approve
+  get    '/reports/new'         => 'reports#new',     :as => :report_new
+  get    '/reports/:id'         => 'reports#show',    :as => :report
+  delete '/reports/:id'         => 'reports#deny',    :as => :report_deny
+  post   '/reports'             => 'reports#create',  :as => :report_create
+  get    '/reports'             => 'reports#index',   :as => :reports
 
-  get 'tools/test' => 'tools#test'
-  get 'tools/hunkerdown' => 'tools#hunkerdown'
-  get 'tools/blockchain' => 'tools#blockchain'
+  get '/tools/test' => 'tools#test'
+  get '/tools/hunkerdown' => 'tools#hunkerdown'
+  get '/tools/blockchain' => 'tools#blockchain'
 
   get  '/signin'                  => 'sessions#new',      :as => :signin
   get  '/signout'                 => 'sessions#destroy',  :as => :signout
