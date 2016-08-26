74b6652cdc8029cc12268e07b0294cbb67ba43a1
Lynn Cyrin <firemagelynn@gmail.com>
2016-08-26 04:26:04 -0700

    prevent database requests when no user_id in session
    (this reverts a recent commit :P)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
     def current_user
-      @current_user = User.find_by id: session.fetch(:user_id, nil)
+      @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
     end
 end
