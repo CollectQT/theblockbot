9d77d9c54ea0dea7be4390e19790f0328c514609
Lynn Cyrin <firemagelynn@gmail.com>
2016-08-26 16:13:30 -0700

    syntax simplification

--- app/workers/create_unblocks_from_expire.rb
+++ app/workers/create_unblocks_from_expire.rb
@@ -10,10 +10,7 @@ class CreateUnblocksFromExpire
       user = block.user
       block.report.update_attributes(expired: true)
 
-      # if user doesn't allow blocks to expire
-      if not user.let_expire
-        next
-      end
+      next unless user.let_expire
 
       # unblock
       PostUnblock.perform_async(user.id, block.target.account_id.to_i, block.id)
