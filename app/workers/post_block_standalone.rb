e9e285816a5f285c66d1f4b6f9238d1e9b32d90e
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-06 17:09:42 -0700

    add standalone block worker

---
+++ app/workers/post_block_standalone.rb
@@ -0,0 +1,14 @@
+class PostBlock
+  include Sidekiq::Worker
+
+  sidekiq_retry_in do |count|
+    # 15min * 60sec/min = 900sec
+    900 * (count + 1)
+  end
+
+  def perform(user_database_id, target_account_id)
+    user = MetaTwitter::Auth.config( User.find(user_database_id) )
+    user.block(target_account_id)
+  end
+
+end
