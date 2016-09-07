e3fab9a9f6ebc0572918c6952dedb09809dc09fe
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-06 22:06:48 -0700

    fix user_model vs user_auth issues in create_block*

--- app/workers/create_block.rb
+++ app/workers/create_block.rb
@@ -2,22 +2,23 @@ class CreateBlock
   include Sidekiq::Worker
 
   def perform(user_database_id, report_id)
-    user = MetaTwitter::Auth.config( User.find(user_database_id) )
+    user_model = User.find(user_database_id)
+    user_auth  = MetaTwitter::Auth.config(user_model)
     report = Report.find(report_id)
     target = report.target.account_id.to_i
 
     # dont block users that our client user is following
-    MetaTwitter.get_following?(user, target) ? return : nil
+    MetaTwitter.get_following?(user_auth, target) ? return : nil
 
     # dont block users that are already blocked
-    MetaTwitter.get_blocked?(user, target) ? return : nil
+    MetaTwitter.get_blocked?(user_auth, target) ? return : nil
 
     # if the user does not want to block followers...
-    if user.dont_block_followers
+    if user_model.dont_block_followers
       # ...then dont block followers
-      MetaTwitter.get_follower?(user, target) ? return : nil
+      MetaTwitter.get_follower?(user_auth, target) ? return : nil
     end
 
-    PostBlock.perform_async(user.id, report.id)
+    PostBlock.perform_async(user_model.id, report.id)
   end
 end
--- app/workers/create_block_standalone.rb
+++ app/workers/create_block_standalone.rb
@@ -2,18 +2,19 @@ class CreateBlockStandalone
   include Sidekiq::Worker
 
   def perform(user_database_id, target_account_id)
-    user = MetaTwitter::Auth.config( User.find(user_database_id) )
+    user_model = User.find(user_database_id)
+    user_auth  = MetaTwitter::Auth.config(user_model)
 
     # dont block users that our client user is following
-    MetaTwitter.get_following?(user, target) ? return : nil
+    MetaTwitter.get_following?(user_auth, target_account_id) ? return : nil
 
     # dont block users that are already blocked
-    MetaTwitter.get_blocked?(user, target) ? return : nil
+    MetaTwitter.get_blocked?(user_auth, target_account_id) ? return : nil
 
     # if the user does not want to block followers...
-    if user.dont_block_followers
+    if user_model.dont_block_followers
       # ...then dont block followers
-      MetaTwitter.get_follower?(user, target) ? return : nil
+      MetaTwitter.get_follower?(user_auth, target_account_id) ? return : nil
     end
 
     PostBlockStandalone.perform_async(user_database_id, target_account_id)
