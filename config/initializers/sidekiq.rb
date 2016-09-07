500e6063bf1f1fd19cc379292bbd5e043bc9adb2
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-06 22:31:06 -0700

    database pooling changes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,11 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
+
+Sidekiq.configure_server do |config|
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=10"
+    ActiveRecord::Base.establish_connection
+  end
+end
