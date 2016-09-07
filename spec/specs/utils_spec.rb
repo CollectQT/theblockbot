063c9e4dd7bd07c71b158666f3cbcbd4877694da
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-07 07:19:42 -0700

    add ulits spec

---
+++ spec/specs/utils_spec.rb
@@ -0,0 +1,12 @@
+describe "send" do
+
+  let(:args) { { user_id: 1, target_id: 2, report_id: 3, block_list_id: 4 } }
+  let(:callback) { JSON.load JSON.dump ['create', args] }
+  let(:model) { Block }
+
+  it "creates a model object with given arguments" do
+    expect(model).to receive(callback[0]).with(callback[1])
+    Utils.send(model, callback)
+  end
+
+end
