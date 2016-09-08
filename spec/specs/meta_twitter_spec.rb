fe042ac5cd4d05f5ad7ede71d730592e74235d2a
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-07 23:28:07 -0700

    add meta_twitter spec

---
+++ spec/specs/meta_twitter_spec.rb
@@ -0,0 +1,16 @@
+describe "meta_twitter" do
+
+  let(:env_user) { MetaTwitter::Auth.config( User.get_from_ENV ) }
+
+  context "MetaTwitter::ReadFollows.from_followers" do
+    it "reads roughly the same about of followers as followers_count", :vcr do
+      follows          = MetaTwitter::ReadFollows.from_followers( env_user, target: '@gitlab' )
+      rounded_follows  = Flt.DecNum(follows.count).round(precision: 1)
+      follow_count     = TwitterClient.user('@gitlab').followers_count
+      rounded_count    = Flt.DecNum(follow_count).round(precision: 1)
+
+      expect(rounded_follows).to eq(rounded_count)
+    end
+  end
+
+end
