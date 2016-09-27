ba84b0b3973368cc44b4f22c32ae09595e7e3472
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-27 07:11:48 -0700

    better factorygirl init

--- spec/factories/block_list.rb
+++ spec/factories/block_list.rb
@@ -1,9 +1,9 @@
 describe "BlockList", :type => :feature  do
 
-  let(:block_list)  { build :block_list }
+  let(:block_list_test)  { build :block_list, name: 'factory test' }
 
   it "utilizes factorygirl" do
-    expect(block_list.name).to eql('factory test')
+    expect(block_list_test.name).to eql('factory test')
   end
 
 end
--- spec/specs/models/block_list_spec.rb
+++ spec/specs/models/block_list_spec.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
+
   factory :block_list do
-    name 'factory test'
   end
+
 end
