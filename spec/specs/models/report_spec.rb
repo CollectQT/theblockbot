52743d9ba42b225b108b8d0fbdca1ba001ad1876
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-28 04:54:12 -0700

    add initial report spec

---
+++ spec/specs/models/report_spec.rb
@@ -0,0 +1,29 @@
+describe "Report" do
+
+  let(:user) { User.get_from_ENV }
+  let(:block_list) {
+    block_list = BlockList.create(name: 'testlist')
+    Admin.create(user: user, block_list: block_list)
+    block_list
+  }
+  let(:report_parent) { Report.parse_objects(
+    block_list: block_list,
+    target: User.first,
+    text: 'parent report',
+    reporter: user,
+  ) }
+  let(:report_child) { Report.parse_objects(
+    block_list: BlockList.first,
+    target: User.first,
+    text: 'child report',
+    reporter: user,
+    parent_id: report_parent.id,
+  ) }
+
+  context 'parse_objects' do
+    it 'creates a report' do
+      expect(report_parent.id).to be_a(Integer)
+    end
+  end
+
+end
