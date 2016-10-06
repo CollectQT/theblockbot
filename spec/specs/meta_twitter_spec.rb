bddea5b95d7a08fb8a51d3827085e9b0ac054c85
Lynn Cyrin <firemagelynn@gmail.com>
2016-10-06 12:27:59 -0700

    update spec

--- spec/specs/meta_twitter_spec.rb
+++ spec/specs/meta_twitter_spec.rb
@@ -2,62 +2,14 @@ describe "MetaTwitter" do
 
   let(:env_user) { MetaTwitter::Auth.config( User.get_from_ENV ) }
 
-  context "::ReadFollows.from_followers" do
+  context ".get_followers" do
     it "reads roughly the same amount of followers as followers_count", :vcr do
-      follows          = MetaTwitter::ReadFollows.from_followers( env_user, target: '@gitlab' )
+      follows          = MetaTwitter.get_followers( env_user, target: '@gitlab' )
       rounded_follows  = Flt.DecNum(follows.count).round(precision: 1)
       follow_count     = TwitterClient.user('@gitlab').followers_count
       rounded_count    = Flt.DecNum(follow_count).round(precision: 1)
 
       expect(rounded_follows).to eq(rounded_count)
-    end
-  end
-
-  let (:stub_get_connections) {
-    allow(MetaTwitter).to receive(:get_connections) do |user, ids|
-      ids.map { |id|
-        case id
-          when 1 then {'id': id, 'connections': 'none'}.to_dot
-          when 2 then {'id': id, 'connections': 'following'}.to_dot
-          when 3 then {'id': id, 'connections': 'none'}.to_dot
-          when 4 then {'id': id, 'connections': 'followed_by'}.to_dot
-        end
-      }.compact
-    end
-  }
-
-  context ".remove_follow_from_list" do
-    it "removes following from list" do
-      stub_get_connections
-
-      list = MetaTwitter.remove_follow_from_list(nil, [1, 2, 3, 4], "following")
-      expected_list = [1, 3, 4]
-
-      expect(list).to eq(expected_list)
-    end
-
-    it "removes followers from list" do
-      stub_get_connections
-
-      list = MetaTwitter.remove_follow_from_list(nil, [1, 2, 3, 4], "followed_by")
-      expected_list = [1, 2, 3]
-
-      expect(list).to eq(expected_list)
-    end
-  end
-
-  let (:stub_block_ids) {
-    allow(MetaTwitter::BlockIds).to receive(:read).and_return([3])
-  }
-
-  context ".remove_blocked_from_list" do
-    it "removes blocked from list" do
-      stub_block_ids
-
-      ids = MetaTwitter.remove_blocked_from_list(nil, [1, 2, 3])
-      expected_ids = [1, 2]
-
-      expect(ids).to eq(expected_ids)
     end
   end
 
